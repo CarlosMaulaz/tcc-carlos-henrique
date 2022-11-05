f023b5dfff1b7da873f43e44196192c71dc34d00
Carlos Henrique <carlosmaulaz@gmail.com>
2022-11-05 10:55:30 -0300

    Novo commit para ver dados no GitLab

--- importGitLab.py
+++ importGitLab.py
@@ -1,46 +1,40 @@
 import gitlab
 import requests
 
-###################################### Acesso ao GitLab ##############################################################
-
-'''
-# anonymous read-only access for public resources (GitLab.com)
-gl = gitlab.Gitlab()
-
-# anonymous read-only access for public resources (self-hosted GitLab instance)
-gl = gitlab.Gitlab('https://gitlab.example.com')
-
-# private token or personal token authentication (GitLab.com)
-gl = gitlab.Gitlab(private_token='glpat-A6cWK7xiqmBHnyxrv3hK')
-'''
+# ##################################### Acesso ao GitLab ##############################################################
 
 # private token or personal token authentication (self-hosted GitLab instance)
 gl = gitlab.Gitlab(private_token='glpat-xoqAg_ka95WfnnP7b3_N')
-'''
-# oauth token authentication
-gl = gitlab.Gitlab('https://gitlab.example.com', oauth_token='my_long_token_here')
 
-# job token authentication (to be used in CI)
-# bear in mind the limitations of the API endpoints it supports:
-# https://docs.gitlab.com/ee/ci/jobs/ci_job_token.html
-import os
-gl = gitlab.Gitlab('https://gitlab.example.com', job_token=os.environ['CI_JOB_TOKEN'])
-
-# Define your own custom user agent for requests
-gl = gitlab.Gitlab('https://gitlab.example.com', user_agent='my-package/1.0.0')
-'''
-# make an API request to create the gl.user object. This is not required but may be useful
-# to validate your token authentication. Note that this will not work with job tokens.
 gl.auth()
+
+# #####################################################################################################################
 
 base_api_url = 'https://gitlab.com/api/v4/'
 user_id1 = 'miikanissi'
 user_id2 = 'carlosmaulaz'
+project_ch = 'Carlos Henrique Maulaz de Freitas / tcc-carlos-henrique'
 
+
+# ############################################# Classes ###############################################################
+
+class Commit:
+    def __init__(self, project_id, commit_id, author_name, author_email, author_date, commiter_name, commiter_email, commiter_date, tree_sha):
+        self.project_id = project_id
+        self.sha = sha
+        self.author_name = author_name
+        self.author_email = author_email
+        self.author_date = author_date
+        self.commiter_name = commiter_name
+        self.commiter_email = commiter_email
+        self.commiter_date = commiter_date
+        self.tree_sha = tree_sha
 
 response = requests.get(base_api_url + '/users/' + user_id2 + '/projects').json()
 
 list_projects = []
+
+#print(response)
 
 for r in response:
     id = r['id']
@@ -48,8 +42,14 @@ for r in response:
     print(id)
 
 
+def get_commits(project_id):
+    response = requests.get(base_api_url + '/projects/' + str(project_id) + '/repository/commits').json()
+    print(response)
+
 # class Projects:
 #     def __init__(self, id, ):
 
 #######################################################################################################################
+# /projects/:id/repository/commits
 
+get_commits(40743609)
